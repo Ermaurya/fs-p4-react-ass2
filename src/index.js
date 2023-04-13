b29894e78dabb6733995915bf4b4c55dfc7329dc
shubham maurya <cs.shubhammaurya@gmail.com>
2023-04-13 20:58:48 +0530

    initial

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import CustomProvider from './components/Contextapi';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
+    <CustomProvider>
+
     <App />
+    </CustomProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
